938e7908a0cbcbc0c23cd6c4f4ea042b870cfc9c
45458976 <112466375+45458976@users.noreply.github.com>
2023-11-26 02:21:19 +0800

    Add files via upload

--- test.sh
+++ test.sh
@@ -1,40 +1,65 @@
 #!/bin/bash
-cd /data/data/com.termux/files/home/
+
+cd
+
+if [ ! -d "SillyTavern" ]; then
+   read -p "未检测到SillyTavern文件夹,是否进行下载(Y/N)" download
+   if [ "$download" == "Y" ] || [ "$download" == "y" ]; then
+      git clone https://github.com/SillyTavern/SillyTavern.git
+      exit
+   elif [ "$download" == "N" ] || [ "$download" == "n" ]; then
+      echo "已取消下载"
+      exit
+   else
+      echo "输入错误"
+      exit
+   fi
+fi
+
+if [ -d "SillyTavern_old" ]; then
+  NEW_FOLDER_NAME="SillyTavern_$(date +%Y%m%d)"
+  mv SillyTavern_old $NEW_FOLDER_NAME
+fi
+
 git clone https://github.com/SillyTavern/SillyTavern.git SillyTavern1
-cp -r /data/data/com.termux/files/home/SillyTavern/public/characters/. /data/data/com.termux/files/home/SillyTavern1/public/characters/
-cp -r /data/data/com.termux/files/home/SillyTavern/public/chats/. /data/data/com.termux/files/home/SillyTavern1/public/chats/
-cp -r /data/data/com.termux/files/home/SillyTavern/public/worlds/. /data/data/com.termux/files/home/SillyTavern1/public/worlds/
-cp -r /data/data/com.termux/files/home/SillyTavern/public/groups/. /data/data/com.termux/files/home/SillyTavern1/public/groups/
-cp -r /data/data/com.termux/files/home/SillyTavern/public/group\ chats/. /data/data/com.termux/files/home/SillyTavern1/public/group\ chats/
-cp -r /data/data/com.termux/files/home/SillyTavern/public/OpenAI\ Settings/. /data/data/com.termux/files/home/SillyTavern1/public/OpenAI\ Settings/
-cp -r /data/data/com.termux/files/home/SillyTavern/public/User\ Avatars/. /data/data/com.termux/files/home/SillyTavern1/public/User\ Avatars/
-cp -r /data/data/com.termux/files/home/SillyTavern/public/backgrounds/. /data/data/com.termux/files/home/SillyTavern1/public/backgrounds/
-mv /data/data/com.termux/files/home/SillyTavern /data/data/com.termux/files/home/SillyTavern_old
-mv /data/data/com.termux/files/home/SillyTavern1 /data/data/com.termux/files/home/SillyTavern
-read -p "是否删除旧版本，请输入Y/N:" para
+cp -r SillyTavern/public/characters/. SillyTavern1/public/characters/
+cp -r SillyTavern/public/chats/. SillyTavern1/public/chats/
+cp -r SillyTavern/public/worlds/. SillyTavern1/public/worlds/
+cp -r SillyTavern/public/groups/. SillyTavern1/public/groups/
+cp -r SillyTavern/public/group\ chats/. SillyTavern1/public/group\ chats/
+cp -r SillyTavern/public/OpenAI\ Settings/. SillyTavern1/public/OpenAI\ Settings/
+cp -r SillyTavern/public/User\ Avatars/. SillyTavern1/public/User\ Avatars/
+cp -r SillyTavern/public/backgrounds/. SillyTavern1/public/backgrounds/
+mv SillyTavern SillyTavern_old
+mv SillyTavern1 SillyTavern
 
-case $para in 
-	[yY])
-        read -p "若要删除请再次确认" queren
+read -p "是否删除旧版本,请输入Y/N:" para
 
-        case $queren in
-            [yY])
-                rm -rfv SillyTavern_old
+case $para in
+        [yY])
+                read -p "若要删除请再次确认" queren
+
+                case $queren in
+                        [yY])
+                                rm -rfv SillyTavern_old
+                                ;;
+                        [nN])
+                                echo "保留旧版本"
+                                ;;
+                        *)
+                                echo "错误的输入"
+                                read -p "保留旧版本"
+                                exit 11
+                                ;;
+                esac
                 ;;
-            [nN])
+        [nN])
                 echo "保留旧版本"
                 ;;
-            *)
-		        echo "错误的输入"
-		        read -p "保留旧版本" exit
-		        exit 1
-                ;;
-	[nN])
-		echo "保留旧版本"
-		;;
-	*)
-		echo "错误的输入"
-		read -p "保留旧版本" exit
-		exit 1
-esac # end case
+        *)
+                echo "错误的输入"
+                read -p "保留旧版本"
+                exit 12
+esac
+
 exit
